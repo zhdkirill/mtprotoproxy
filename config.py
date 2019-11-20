882494d68fda6ae90ab07081baf9671ea052a402
Kirill Zhdanov <kzhdanov@mirantis.com>
2019-11-20 16:25:25 +0100

    nginx server and minor modifications

--- config.py
+++ config.py
@@ -6,6 +6,9 @@ USERS = {
 #    "tg2": "0123456789abcdef0123456789abcdef",
 }
 
+# Enables proxy
+PROXY_PROTOCOL = True
+
 # Makes the proxy harder to detect
 # Can be incompatible with very old clients
 SECURE_ONLY = True
@@ -16,7 +19,7 @@ TLS_ONLY = True
 
 # The domain for TLS, bad clients are proxied there
 # Use random existing domain, proxy checks it on start
-TLS_DOMAIN = "www.google.com"
+TLS_DOMAIN = "mtproxy-tls.sharik.network"
 
 # Tag for advertising, obtainable from @MTProxybot
 # AD_TAG = "3c09c680b76ee91a4c25ad51f742267d"
