606887c5fe3d652f1cf81a4fc6bedbc218cf5cdb
Kirill Zhdanov <kzhdanov@mirantis.com>
2019-11-20 13:48:50 +0100

    initial setup

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ PORT = 443
 
 # name -> secret (32 hex chars)
 USERS = {
-    "tg":  "00000000000000000000000000000001",
+    "tg":  "6f27206d00f11fce53f1e9c62264dd97",
 #    "tg2": "0123456789abcdef0123456789abcdef",
 }
 
@@ -16,7 +16,7 @@ TLS_ONLY = True
 
 # The domain for TLS, bad clients are proxied there
 # Use random existing domain, proxy checks it on start
-# TLS_DOMAIN = "www.google.com"
+TLS_DOMAIN = "www.google.com"
 
 # Tag for advertising, obtainable from @MTProxybot
 # AD_TAG = "3c09c680b76ee91a4c25ad51f742267d"
