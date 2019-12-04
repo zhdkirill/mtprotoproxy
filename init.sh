7340153f657db5631aaaae1a3a6d5fa631ed970f
Kirill Zhdanov <kzhdanov@mirantis.com>
2019-12-04 14:44:35 +0100

    add cron job to renew cert

--- init.sh
+++ init.sh
@@ -7,3 +7,6 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt update; apt install docker-ce docker-ce-cli containerd.io
 certbot certonly --preferred-challenges http --must-staple --redirect --hsts --uir --staple-ocsp --rsa-key-size 4096 --domain $DOMAIN
+croncmd="certbot renew"
+cronjob="0 1 * * * $croncmd"
+( crontab -l | grep -v -F "$croncmd" ; echo "$cronjob" ) | crontab -
