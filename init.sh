313e362a8e72ef4452d667fc33d8339ff3090edd
Kirill Zhdanov <kzhdanov@mirantis.com>
2019-12-04 17:34:43 +0100

    ndppd for ipv6

--- init.sh
+++ init.sh
@@ -2,10 +2,11 @@
 DOMAIN=mtproxy-tls.sharik.network
 apt remove -y docker docker-engine docker.io containerd runc
 apt update
-apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common ndppd
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt update; apt install docker-ce docker-ce-cli containerd.io
+sysctl net.ipv6.conf.eth0.proxy_ndp=1
 certbot certonly --preferred-challenges http --must-staple --redirect --hsts --uir --staple-ocsp --rsa-key-size 4096 --domain $DOMAIN
 croncmd="certbot renew"
 cronjob="0 1 * * * $croncmd"
